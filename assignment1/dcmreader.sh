bc40dfe20afc574da3f202440651ef1745a283ff
hkim171 <hkim171@Jhu.edu>
2019-02-03 21:42:58 +0000

    finished assignment 1 added

--- assignment1/dcmreader.sh
+++ assignment1/dcmreader.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# first argument takes in search PATH. Second argument takes in the output file name. 
+# Looks for .dcm directory in PATH specified and writes it to the output file as a .txt. 
+
 touch "${2}.txt"
-ls *.dcm > "${2}.txt"
+ls $1*.dcm > "${2}.txt"
 
