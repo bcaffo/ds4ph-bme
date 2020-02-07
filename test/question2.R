f00acfd73fa7c84f2109dd8cd4324fbb147e7e79
JialinKang <jkang58@jhu.edu>
2020-02-07 02:11:10 +0000

    question2.R

---
+++ test/question2.R
@@ -0,0 +1,21 @@
+webpage <- function(){
+    # this function is used to count the lines in a file which we get it
+    # by run the command 'wget -O alex.txt https://en.wikipedia.org/wiki/Alexander_the_Great'
+    
+    # Parameter
+    # when run this function, you need the path of the file
+    # Return
+    # i:interge
+    # the number of lines
+    # the answer of this file is 3692
+  
+    i = 1
+    f <- file('/cloud/project/ds4ph-bme/test/alex.txt', open = 'rt')
+    while(TRUE){
+      lines <- readLines(f, 1)
+      if(length(lines)==0) break
+      else i = i + 1
+    }
+    close(f)
+    return(i)
+}
