95c65ce10537c36846263120ff6f5e61f582d229
JialinKang <jkang58@jhu.edu>
2020-02-07 15:44:48 +0000

    question6.R

---
+++ test/question6.R
@@ -0,0 +1,15 @@
+n = readline('piease input the number of the peoples in the romm:')
+n = as.integer(n)
+if(n<365){
+  p = 1 - prod((366-n):365)/365^n
+}else{
+  p = 0
+}
+arr = c()
+for(i in 1:365){
+  p = 1 - prod((366-i):365)/365^i
+  if(p == 'NaN') break
+  arr[i] <- p
+}
+plot(1:length(arr), arr, main = 'The computed probability of at least two people sharing a birthday',
+     xlab = 'number of people', ylab = 'probability')
